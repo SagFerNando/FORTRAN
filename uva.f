af8f826a37699aa57ad92a74210ddd4a761738ad
SagFerNando <111102672+SagFerNando@users.noreply.github.com>
2022-09-28 20:28:16 -0500

    Create uva.f

--- uva.f
+++ uva.f
@@ -1,32 +1,48 @@
- program uva
-        integer tamano
+        program uva
+        implicit none
+        integer tamano, kilo
+        real precio
         character(1) tipo
-        character(20) precio
+
+
         
-        write(*,*)'¨De que tipo es la uva, A o B?'
-        read*, tipo
-        write(*,*)'¨De que tama¤o es la uva, 1 o 2?'
-        read*, tamano
+        write(*,*) 'Costo de la uva?'
+        print *
+        read(*,*) precio
+        write(*,*) 'Cuantos kilon compra?'
+        print *
+        read(*,*) kilo
+        write(*,*) 'Tipo de Uva A y B?'
+        print *
+        read(*,*) tipo
+        write(*,*) 'Tamano de la uva 1 y 2?'
+        print *
+        read(*,*) tamano
         
         if(tipo=='a') then
-                      if(tamano==1) then
-                                    precio='mas 20 centavos'
-                      else if(tamano==2) then
-                                    precio='mas 30 centavos'
-                      else
-                                    write(*,*)'solo hay tipo 1 y 2'
-                      end if
+                if(tamano==1) then
+                            precio=precio+0.20
+
+                else if (tamano==2) then
+                            precio=precio+0.30
+
+                else
+                            print *, 'Incorrecto'
+                end if
         else if(tipo=='b') then
-                      if(tamano==1) then
-                                    precio='menos 30 centavos'
-                      else if(tamano==2) then
-                                    precio='menos 50 centavos'
-                      else
-                                    write(*,*)'solo hay tipo 1 y 2'
-                      end if
-         else
-                      write(*,*)'solo hay tipo a y b'
-         end if
-         write(*,*)'A su precio se hacen ',precio
-         pause
-         end program uva
+                if(tamano==1) then
+                            precio=precio-0.30
+
+                else if (tamano==2) then
+                            precio=precio-0.50
+
+                else
+                            print *, 'Incorrecto'
+                end if
+        else
+                print *, 'Incorrecto'
+        end if
+        precio=precio*kilo
+        write(*,*) 'El precio tota es ',precio
+        pause
+        end program uva
